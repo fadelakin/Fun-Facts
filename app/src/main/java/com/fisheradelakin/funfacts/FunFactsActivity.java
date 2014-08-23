d8151cd14606d56155cb340c6b2764fba8b19add
Fisher Adelakin <temidayoadelakin@gmail.com>
2014-08-22 20:41:17 -0400

    random generation

--- app/src/main/java/com/fisheradelakin/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/fisheradelakin/funfacts/FunFactsActivity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 public class FunFactsActivity extends Activity {
@@ -12,6 +17,24 @@ public class FunFactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fun_facts);
+
+        // Declare view variables and assign views
+        final TextView factLabel = (TextView) findViewById(R.id.factTextView);
+        Button showFactButton = (Button) findViewById(R.id.showFactButton);
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Button was clicked, so update with a new fact
+                String fact = "";
+                // Randomly select a fact
+                Random randomGenerator = new Random(); // random number generator
+                int randomNumber = randomGenerator.nextInt(3);
+                fact = randomNumber + "";
+                // Update label with random fact
+                factLabel.setText(fact);
+            }
+        };
+        showFactButton.setOnClickListener(listener);
     }
 
 
